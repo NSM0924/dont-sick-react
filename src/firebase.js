3190177b08b674ba613fa633dd43a802e3421188
NSM0924 <nasungmin50@naver.com>
2021-11-19 02:22:04 +0900

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { getMessaging } from "@firebase/messaging";
+import { getMessaging, getToken } from "@firebase/messaging";
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
