ab52a2e19b9a80f661a049b7c7c1ae506ab13114
NSM0924 <nasungmin50@naver.com>
2021-11-19 03:22:19 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -40,11 +40,13 @@ function App() {
         setName(user.displayName);
 
         // 알림 토큰 저장
-        const serviceWorkerRegistration = navigator.serviceWorker.register(
-          "/dont-sick-react/firebase-messaging-sw.js"
-        );
+        const serviceWorker = () => {
+          navigator.serviceWorker.register(
+            "/dont-sick-react/firebase-messaging-sw.js"
+          );
+        };
         getToken(messaging, {
-          serviceWorkerRegistration,
+          serviceWorkerRegistration: serviceWorker,
           vapidKey:
             "BLnmZ7MoMERjyVHv4b791C7j1_-xqcVi9aCrVWDDFovZSGDgK9FROae3J8Q7AWqTJwbQDc2Dk4LrU0zAEUVqfVQ",
         })
